31fe194d4a934bd804fb2a18739710808296f5c5
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2024-08-21 11:42:36 +0200

    document Animation class

--- src/Animation.py
+++ src/Animation.py
@@ -4,15 +4,38 @@ from src.ImageManager import ImageManager
 
 vec = pygame.math.Vector2  # 2 for two dimensional
 
+# Describes the information needed for an animation
+# Used in the ctor of Animation
+# Requirements :
+# 1. All the frames of the animation must be contained and be aligned in a single file (a spritesheet)
+# 2. The file must be loaded in the ImageManager
 class SpritesheetAnimInfos():
-    def __init__(self, textureManagerKey, frameSize: vec, origin: vec, frameCount, animationTime):
-        self.key = textureManagerKey
+    # textureManagerKey : key of the texture in the ImageManager
+    # frameSize : size of a single frame on the sprite sheet
+    # origin : top left coordinates of the first frame on the spritesheet 
+    # frameCount : total number of frames in the animation
+    # animationTime : time needed for a single loop of the animation to complete, reduce to increase fps
+    def __init__(self, imageManagerKey, frameSize: vec, origin: vec, frameCount, animationTime):
+        self.key = imageManagerKey
         self.frameSize = frameSize
         self.origin = origin
         self.frameCount = frameCount
         self.animationTime = animationTime
 
+# Represents an Animation that can be drawn on a surface
+# Example usage :
+#
+# infos = SpritesheetAnimInfos('player_idle', vec(32,32), vec(0,0), 4, 0.5)
+# anim_test = Animation(infos, vec(100,100), vec(80,80))
+# anim_test.start()
+#
+# ... in the main loop ...
+# anim_test.update(0.5)
+#
+# anim_test.draw(displaysurface)
 class Animation():
+    # todo : add methods to flip animation horizontally and vertically
+
     def __init__(self, infos: SpritesheetAnimInfos, position: vec, size: vec):
         self.surfaces = list()
         self.infos = infos
@@ -40,6 +63,7 @@ class Animation():
     def stop(self):
         self.isRunning = False
 
+    # updates the animation depending on the time elapsed since the last call to update()
     def update(self, dt: float):
         if not self.isRunning:
             return
@@ -49,6 +73,7 @@ class Animation():
             self.goto_next_frame()
             self.timeSinceStart -= timePerFrame
 
+    # goes to next frame or loops if the animation is over
     def goto_next_frame(self):
         self.spriteIndex = (self.spriteIndex + 1) % self.infos.frameCount
 
